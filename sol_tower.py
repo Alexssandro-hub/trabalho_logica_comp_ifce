6fa2303a9c20efe21530e369d5ea1fd25e2edfc4
Alexssandro Tabosa <71848388+Alexssandro-hub@users.noreply.github.com>
2023-12-02 21:26:12 -0300

    Update sol_tower.py
    alterações das restrições e matriz de saída desejada, falta parametrizar a saída com os números específicos.

--- sol_tower.py
+++ sol_tower.py
@@ -24,22 +24,20 @@ def solve_tower_problem(dimensions, configuration):
     for i in range(rows):
       for j in range(cols):
           if configuration[i][j] == 'T':
-              #s.add(Or(towers_left[i][j], towers_down[i][j], towers_right[i][j], towers_up[i][j]))
-
               # Cada configuração de tiro deve eliminar pelo menos um atacante
               attackers_in_row = Or([towers_left[k][j] for k in range(rows) if k != i] + [towers_right[k][j] for k in range(rows) if k != i])
               attackers_in_col = Or([towers_up[i][k] for k in range(cols) if k != j] + [towers_down[i][k] for k in range(cols) if k != j])
 
-              s.add(And(Not(configuration[i][j] == 'n'), Or(attackers_in_row, attackers_in_col))); #Implies(configuration[i][j] == 'n', Or(attackers_in_row, attackers_in_col))
+              s.add(And(Not(configuration[i][j] == 'n'), Or(attackers_in_row, attackers_in_col)));
 
               # Restrições para evitar que as torres se destruam mutuamente
               neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
               for ni, nj in neighbors:
                   if 0 <= ni < rows and 0 <= nj < cols and configuration[ni][nj] == 'T':
-                      s.add(Implies(towers_left[i][j], Not(towers_right[ni][nj])))
-                      s.add(Implies(towers_down[i][j], Not(towers_up[ni][nj])))
-                      s.add(Implies(towers_right[i][j], Not(towers_left[ni][nj])))
-                      s.add(Implies(towers_up[i][j], Not(towers_down[ni][nj])))
+                      s.add(Implies(And(towers_left[i][j], towers_down[i][j]), Not(And(towers_right[ni][nj], towers_down[ni][nj])))) #towers_left[i][j], towers_right[ni][nj]
+                      s.add(Implies(And(towers_down[i][j], towers_right[i][j]), Not(And(towers_up[ni][nj], towers_left[ni][nj])))) #towers_down[i][j], towers_up[ni][nj]
+                      s.add(Implies(And(towers_right[i][j], towers_up[i][j]), Not(And(towers_left[ni][nj], towers_down[ni][nj])))) #towers_right[i][j], towers_left[ni][nj]
+                      s.add(Implies(And(towers_up[i][j], towers_left[i][j]), Not(And(towers_down[ni][nj], towers_right[ni][nj])))) #towers_up[i][j], towers_down[ni][nj]
 
     # Verifica se é possível satisfazer as restrições
     check_result = s.check()
@@ -48,16 +46,17 @@ def solve_tower_problem(dimensions, configuration):
         model = s.model()
         # Gera a saída do jogo
         solution = np.full((rows, cols), '.', dtype=str)
+        
         for i in range(rows):
             for j in range(cols):
                 if configuration[i][j] == 'T':
-                    if is_true(model.eval(towers_left[i][j] > 0)):
+                    if is_true(model.eval(towers_down[i][j])) or is_true(model.eval(towers_left[i][j])):
                         solution[i][j] = '1'
-                    elif is_true(model.eval(towers_down[i][j] > 0)):
+                    if is_true(model.eval(towers_right[i][j])) or is_true(model.eval(towers_down[i][j])):
                         solution[i][j] = '2'
-                    elif is_true(model.eval(towers_right[i][j] > 0)):
+                    if is_true(model.eval(towers_up[i][j])) or is_true(model.eval(towers_right[i][j])):
                         solution[i][j] = '3'
-                    elif is_true(model.eval(towers_up[i][j] > 0)):
+                    if is_true(model.eval(towers_left[i][j])) or is_true(model.eval(towers_up[i][j])):
                         solution[i][j] = '4'
                 elif configuration[i][j] == '#':
                     solution[i][j] = '#'
